df20507c995236a3579f4af968399f0423a7f5ed
Joeymondello13 <38968221+Joeymondello13@users.noreply.github.com>
2019-01-29 14:26:14 -0600

    Added code to script

--- Hello World!!/Assets/Scenes/Johnson.cs
+++ Hello World!!/Assets/Scenes/Johnson.cs
@@ -7,7 +7,8 @@ public class Johnson : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // I added this code for you to pull
+        Debug.Log("Hello World!");
     }
 
     // Update is called once per frame
