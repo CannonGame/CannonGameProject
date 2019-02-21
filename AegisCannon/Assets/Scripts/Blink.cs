ea4ffb235cd5b6a327c17017ab09cb0321a98bb8
Joeymondello13 <38968221+Joeymondello13@users.noreply.github.com>
2019-02-21 14:41:01 -0600

    Ticket #163850304

---
+++ AegisCannon/Assets/Scripts/Blink.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blink : MonoBehaviour
+{
+
+    // UI Element To Toggle
+    public MaskableGraphic imageToToggle;
+
+    public float interval = 1f;
+    public float startDelay = 0.5f;
+    public bool currentState = true;
+    public bool defaultState = true;
+    bool isBlinking = false;
+
+
+    void Start()
+    {
+        imageToToggle.enabled = defaultState;
+        StartBlink();
+    }
+
+    // Method to make button blink.
+    public void StartBlink()
+    {
+        
+        if (isBlinking)
+            return;
+
+        if (imageToToggle != null)
+        {
+            isBlinking = true;
+            InvokeRepeating("ToggleState", startDelay, interval);
+        }
+
+    }
+
+    public void ToggleState()
+    {
+        imageToToggle.enabled = !imageToToggle.enabled;
+
+    }
+
+}
---
+++ AegisCannon/Assets/Scripts/StartGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // This will lead to the Main Menu Scene once implemented.
+        if (Input.anyKeyDown || Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Level Select");
+        }
+    }
+}
