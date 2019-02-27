53d3858003faa82936829a055aaf6242cbed25b9
jjohnson0022 <46877862+jjohnson0022@users.noreply.github.com>
2019-02-26 21:08:38 -0600

    Home Screen Button Functionality
    #163850333, #163850343, #163850354

---
+++ AegisCannon/Assets/Scripts/HomeScreenButtonClicks.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Script for Home Screen Button Interactions
+
+public class HomeScreenButtonClicks : MonoBehaviour
+{
+    // When the Start button is hit, the SelectDifficulty scene is loaded.
+    public void StartButtonClick()
+    {
+        SceneManager.LoadScene("SelectDifficulty");
+        //Test with Start Screen until other scenes are ready
+        //SceneManager.LoadScene("StartScreen");
+    }
+
+    // When the Options button is hit, the Options scene is loaded.
+    public void OptionsButtonClick()
+    {
+        SceneManager.LoadScene("Options");
+        //Test with Start Screen until other scenes are ready
+        //SceneManager.LoadScene("StartScreen");
+    }
+
+    // When the Credits button is hit, the Credits scene is loaded.
+    public void CreditsButtonClick()
+    {
+        SceneManager.LoadScene("Credits");
+        //Test with Start Screen until other scenes are ready
+        //SceneManager.LoadScene("StartScreen");
+    }
+}
