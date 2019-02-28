f10492f64057a5ca97a87d159303ecd58bb649d1
Joeymondello13 <38968221+Joeymondello13@users.noreply.github.com>
2019-02-28 16:23:17 -0600

    Added Button Script

---
+++ AegisCannon/Assets/Scripts/SelectDifficultyButtons.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectDifficultyButtons : MonoBehaviour
+{
+    // Loads Easy Mode Wave 1.
+    public void EasyButton()
+    {
+
+        SceneManager.LoadScene("Easy Wave 1");
+
+    }
+
+    // Loads Medium Mode Wave 1.
+    public void MediumButton()
+    {
+
+        SceneManager.LoadScene("Medium Wave 1");
+
+    }
+
+    // Loads Hard Mode Wave 1.
+    public void HardButton()
+    {
+
+        SceneManager.LoadScene("Hard Wave 1");
+
+    }
+
+    // Loads Endless Mode Wave 1.
+    public void EndlessButton()
+    {
+
+        SceneManager.LoadScene("Endless Wave 1");
+
+    }
+}
