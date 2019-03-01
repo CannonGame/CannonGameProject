71bb71df476f359c5563235a6361773371ba1839
jjohnson0022 <46877862+jjohnson0022@users.noreply.github.com>
2019-03-01 09:15:32 -0600

    #163851785, #163851789, #164328338

---
+++ AegisCannon/Assets/Scripts/CreditsScreen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CreditsScreen : MonoBehaviour
+{
+    //When Home button is hit it takes you to the home screen
+    public void HomeButtonClick()
+    {
+        SceneManager.LoadScene("HomeScreen");
+    }
+}
