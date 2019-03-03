e9e05278a69545ed751a74e9bf763659baf1700d
jjohnson0022 <46877862+jjohnson0022@users.noreply.github.com>
2019-03-03 13:33:10 -0600

    #164356431, #164356434, #164356440

---
+++ AegisCannon/Assets/Scripts/WinScreenButtons.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreenButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //When Home button is hit it takes you to the home screen
+    public void HomeButtonClick()
+    {
+        SceneManager.LoadScene("HomeScreen");
+    }
+
+    //When Exit button is hit it exits the gamen
+    public void ExitButtonClick()
+    {
+        Application.Quit();
+    }
+}
