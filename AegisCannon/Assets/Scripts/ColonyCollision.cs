e742cff7c3657f3a8b14d20b44c32d44200005a2
jjohnson0022 <46877862+jjohnson0022@users.noreply.github.com>
2019-03-10 01:34:06 -0600

    #163850278, #163850269

---
+++ AegisCannon/Assets/Scripts/ColonyCollision.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColonyCollision : MonoBehaviour
+{
+    // Fields
+    public Sprite spaceColony;
+    public Sprite spaceColonyHit;
+    public Sprite shieldCharge;
+    float timer = 1f;
+    float delay = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If shield is true change colony to turqoise
+        if (ShieldCollision.GetShield())
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = shieldCharge;
+        }
+
+        // When colony changes, start timer
+        if(this.gameObject.GetComponent<SpriteRenderer>().sprite == spaceColonyHit || 
+            this.gameObject.GetComponent<SpriteRenderer>().sprite == shieldCharge)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        // When timer is less than or equal to zero, change colony back
+        if(timer <= 0)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = spaceColony;
+            timer = delay;
+        }
+    }
+
+    //When an enemy bullet collides with colony, health is lost and colony flashes red.
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //Debug.Log("HIT");
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = spaceColonyHit;
+        timer = delay;
+    }
+
+
+}
---
+++ AegisCannon/Assets/Scripts/ShieldCollision.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldCollision : MonoBehaviour
+{
+    // Fields
+    static bool shield = false;
+    float timer = 1f;
+    float delay = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Start timer
+        if (shield)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        //Reset timer and shield state
+        if (timer <= 0)
+        {
+            shield = false;
+            timer = delay;
+        }
+    }
+
+    // When enemy bullet collides with shield, the shield is charged.
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //Debug.Log("Blocked");
+        shield = true;
+        timer = delay;
+    }
+    
+    public static bool GetShield()
+    {
+        return shield;
+    }
+}
