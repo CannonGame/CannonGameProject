c705997b68eee1785bc6aa5823e9b6445127d54b
jjohnson0022 <46877862+jjohnson0022@users.noreply.github.com>
2019-03-09 21:52:48 -0600

    Player entities in. Need Joey's code to test properly.

---
+++ AegisCannon/Assets/ColonyCollision.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColonyCollision : MonoBehaviour
+{
+    //Add Sprite Renderer
+    void OnAwake()
+    {
+        SpriteRenderer sprRend = gameObject.AddComponent<SpriteRenderer>() as SpriteRenderer;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Runs when an object (enemy bullet) collides with the colony. Health drops 1. Colony turns red temporarily.
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //Debug.Log("HIT");
+        gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("SpaceColonyHit");
+        gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("SpaceColony");
+
+    }
+}
---
+++ AegisCannon/Assets/ShieldCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Runs when an object (enemy bullet) collides with the shield. Shield gets charged. Colony turns turqiose temporarily.
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //Debug.Log("BLOCKED");
+    }
+}
