c0899660d0c2f39f8b0144bc946a335932cead2b
Joeymondello13 <38968221+Joeymondello13@users.noreply.github.com>
2019-03-23 18:30:58 -0500

    Tickets: #164357052 #164357049 #164357051 #163852622 #163852619 #163852613 #163852609 #163850329 #163850336

--- AegisCannon/Assets/Scripts/SelectDifficultyButtons.cs
+++ AegisCannon/Assets/Scripts/SelectDifficultyButtons.cs
@@ -8,7 +8,7 @@ public class SelectDifficultyButtons: MonoBehaviour
     // Loads Easy Mode Wave 1. Changes static variables to adjust difficulty.
     public void EasyButton()
     {
-        SceneManager.LoadScene("Wave 1");
+        SceneManager.LoadScene("Easy Wave 1");
         EnemyFire.randomMin = 5f;
         EnemyFire.randomMax = 10f;
         EnemyFire.projectileSpeed = 3f;
@@ -22,18 +22,40 @@ public class SelectDifficultyButtons: MonoBehaviour
     public void MediumButton()
     {
         SceneManager.LoadScene("Medium Wave 1");
+        EnemyFire.randomMin = 4f;
+        EnemyFire.randomMax = 8f;
+        EnemyFire.projectileSpeed = 4f;
+        EnergyBar.maxHealth = 175f;
+        EnergyBar.maxEnergy = 175f;
+        EnergyBar.damage = 12f;
+        EnergyBar.heal = 10f;
+
     }
 
     // Loads Hard Mode Wave 1.
     public void HardButton()
     { 
         SceneManager.LoadScene("Hard Wave 1");
+        EnemyFire.randomMin = 3f;
+        EnemyFire.randomMax = 7f;
+        EnemyFire.projectileSpeed = 4.5f;
+        EnergyBar.maxHealth = 150f;
+        EnergyBar.maxEnergy = 150f;
+        EnergyBar.damage = 15f;
+        EnergyBar.heal = 10f;
     }
 
     // Loads Endless Mode Wave 1.
     public void EndlessButton()
     {
         SceneManager.LoadScene("Endless Wave 1");
+        EnemyFire.randomMin = 5f;
+        EnemyFire.randomMax = 10f;
+        EnemyFire.projectileSpeed = 3.5f;
+        EnergyBar.maxHealth = 200f;
+        EnergyBar.maxEnergy = 200f;
+        EnergyBar.damage = 10f;
+        EnergyBar.heal = 10f;
     }
 
     //When Home button is hit it takes you to the home screen
