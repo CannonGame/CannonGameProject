a6d3bd713d390b12b9fe69150c8f5640af8f1e18
Joeymondello13 <38968221+Joeymondello13@users.noreply.github.com>
2019-03-28 16:36:38 -0500

    Prevent more than 1 enemy from firing at a time.

--- AegisCannon/Assets/Scripts/EnemyFire.cs
+++ AegisCannon/Assets/Scripts/EnemyFire.cs
@@ -12,7 +12,9 @@ public class EnemyFire : MonoBehaviour
     public static float projectileSpeed = 3f;
     public Transform shipTransform;
     public Rigidbody2D projectileBody;
-
+    public bool preparingToFire = false;
+    public bool firing = false;
+    public bool shouldFire = true;
 
     // Start is called before the first frame update. Gets Animator Component and Starts infinite Coroutine to make enemies fire.
     void Start()
@@ -27,18 +29,64 @@ public class EnemyFire : MonoBehaviour
         Rigidbody2D projectile1 = Instantiate(projectileBody, shipTransform.position, shipTransform.rotation);
         projectile1.velocity = transform.up * projectileSpeed;
     }
+    // Called once per frame. Checks which enemy ship was assigned to fire. Sets all other enemy ships to not fire.
+    void Update()
+    {
+        if (GameObject.Find("EnemyShip").GetComponent<EnemyFire>().preparingToFire)
+        {
+            GameObject.Find("EnemyShip").GetComponent<EnemyFire>().firing = true;
+            GameObject.Find("EnemyShip (1)").GetComponent<EnemyFire>().firing = false;
+            GameObject.Find("EnemyShip (2)").GetComponent<EnemyFire>().firing = false;
+            GameObject.Find("EnemyShip (3)").GetComponent<EnemyFire>().firing = false;
+        }
+        if (GameObject.Find("EnemyShip (1)").GetComponent<EnemyFire>().preparingToFire)
+        {
+            GameObject.Find("EnemyShip (1)").GetComponent<EnemyFire>().firing = true;
+            GameObject.Find("EnemyShip").GetComponent<EnemyFire>().firing = false;
+            GameObject.Find("EnemyShip (2)").GetComponent<EnemyFire>().firing = false;
+            GameObject.Find("EnemyShip (3)").GetComponent<EnemyFire>().firing = false;
+        }
+        if (GameObject.Find("EnemyShip (2)").GetComponent<EnemyFire>().preparingToFire)
+        {
+            GameObject.Find("EnemyShip (2)").GetComponent<EnemyFire>().firing = true;
+            GameObject.Find("EnemyShip").GetComponent<EnemyFire>().firing = false;
+            GameObject.Find("EnemyShip (1)").GetComponent<EnemyFire>().firing = false;
+            GameObject.Find("EnemyShip (3)").GetComponent<EnemyFire>().firing = false;
+        }
+        if (GameObject.Find("EnemyShip (3)").GetComponent<EnemyFire>().preparingToFire)
+        {
+            GameObject.Find("EnemyShip (3)").GetComponent<EnemyFire>().firing = true;
+            GameObject.Find("EnemyShip").GetComponent<EnemyFire>().firing = false;
+            GameObject.Find("EnemyShip (2)").GetComponent<EnemyFire>().firing = false;
+            GameObject.Find("EnemyShip (1)").GetComponent<EnemyFire>().firing = false;
+        }
+    }
 
-     /* Infinitely runs this coroutine. Waits a random amount of time, sets the Animator component bool "Firing" to true, which triggers the blinking animation. Waits
-        duration of the variable delay and sets the "Firing" bool to false to turn off the animation. Then calls FireShot to make enemy fire a projectile. */
+
+     /* Infinitely runs this coroutine. Waits a random amount of time, sets the Animator component bool "Firing" to true if the ships shouldFire bool is true and firing bool is true. 
+        Triggers the blinking animation. Waits duration of the variable delay and sets the "Firing" bool to false to turn off the animation. Then calls FireShot to make enemy fire a projectile.
+        sets preparingToFire, firing, and shouldFire to false, waits random amount of time, then sets shouldFire back to true. This way we can control multiple ships with this one script*/
     public IEnumerator StartFire()
     {
         while (true)
         {
-            yield return new WaitForSeconds(Random.Range(randomMin, randomMax));
-            animator.SetBool("Firing", true);
-            yield return new WaitForSeconds(delay);
-            animator.SetBool("Firing", false);
-            FireShot();
+            if (shouldFire)
+            {
+                yield return new WaitForSeconds(Random.Range(randomMin, randomMax));
+                preparingToFire = true;
+                if (firing)
+                {
+                    animator.SetBool("Firing", true);
+                    yield return new WaitForSeconds(delay);
+                    animator.SetBool("Firing", false);
+                    FireShot();
+                    firing = false;
+                    shouldFire = false;
+                    preparingToFire = false;
+                    yield return new WaitForSeconds(Random.Range(5,15));
+                    shouldFire = true;
+                }
+            }
         }
     }
 }
