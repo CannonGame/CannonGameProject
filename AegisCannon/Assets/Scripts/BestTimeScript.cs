2431a8632030ec496b3b51729ead40a14a1132be
dmccomic <dustin.mccomic1@gmail.com>
2019-04-05 09:39:27 -0500

    Best Time Timer

---
+++ AegisCannon/Assets/Scripts/BestTimeScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Internal;
+
+public class BestTimeScript : MonoBehaviour
+{
+    // Variables created
+    public Text timerText;
+    public Text bestTime;
+    private float startTime;
+    private bool finished = false;
+
+    // Creates and displays best time
+    void GameFinished()
+    {
+        float t = Time.time - startTime;
+        if (t < PlayerPrefs.GetFloat ("Best Time", float.MaxValue))
+        {
+            PlayerPrefs.SetFloat("Best Time", t);
+            bestTime.text = t.ToString();
+            PlayerPrefs.Save();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+        bestTime.text = PlayerPrefs.GetFloat("Best Time", 0).ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Timer
+        float t = Time.time - startTime;
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+        timerText.text = minutes + ":" + seconds;
+        
+        // Checks to see if the player finished the game
+        GameFinished();
+    }
+}
--- AegisCannon/Assets/Scripts/GameTimer.cs
+++ AegisCannon/Assets/Scripts/GameTimer.cs
@@ -9,13 +9,16 @@ public class GameTimer : MonoBehaviour
     float gameTimer=0f;
 
     // Update is called once per frame
+    //Creates Timer
     void Update(){
         gameTimer += Time.deltaTime;
 
+        // Creates variables for seconds, minutes and hours
         int seconds = (int)(gameTimer % 60);
         int minutes = (int)(gameTimer / 60) % 60;
         int hours = (int)(gameTimer / 3600) % 24;
 
+        // Formats timer to hours : minutes : seconds
         string timerString = string.Format("{0:0}:{1:00}:{2:00}", hours, minutes, seconds);
         gameTimerText.text = timerString;
     }
