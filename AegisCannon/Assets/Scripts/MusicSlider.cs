0b7abfc39f6b20a9fcaafea4868c2a4ca722eb18
dmccomic <dustin.mccomic1@gmail.com>
2019-04-14 17:49:42 -0500

    test

---
+++ AegisCannon/Assets/Scripts/MusicSlider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicSlider : MonoBehaviour
+{
+    //Creates objects
+    public Slider musicVolume;
+    public AudioSource myMusic;
+    public AudioSource myMusic1;
+    public AudioSource myMusic2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        myMusic.volume = musicVolume.value;
+        myMusic1.volume = musicVolume.value;
+        myMusic2.volume = musicVolume.value;
+    }
+}
---
+++ AegisCannon/Assets/Scripts/SFXSlider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SFXSlider : MonoBehaviour
+{
+    public Slider SFXVolume;
+    public AudioSource mySFX;
+    public AudioSource mySFX1;
+    public AudioSource mySFX2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mySFX.volume = SFXVolume.value;
+        mySFX1.volume = SFXVolume.value;
+        mySFX2.volume = SFXVolume.value;
+    }
+}
