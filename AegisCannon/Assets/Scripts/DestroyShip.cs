58258e493556d55db3a76fad03be15bbc4694aa5
Joeymondello13 <38968221+Joeymondello13@users.noreply.github.com>
2019-04-27 12:29:22 -0500

    Sound Fixes

--- AegisCannon/Assets/Scripts/DestroyShip.cs
+++ AegisCannon/Assets/Scripts/DestroyShip.cs
@@ -7,12 +7,13 @@ public class DestroyShip : MonoBehaviour
     Animator animator;
     public GameObject energyCircle;
     //SFX object - DH
-    SFXscript destroyEnemyShip = GameObject.FindObjectOfType(typeof(SFXscript)) as SFXscript;
+    SFXscript destroyEnemyShip;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        destroyEnemyShip = GameObject.FindObjectOfType(typeof(SFXscript)) as SFXscript;
     }
     // Method to destroy ships on pulse wave impact.
     void OnCollisionEnter2D(Collision2D col)
