dcc0f28662140353009b257238fc3d4bc35be3dd
Joeymondello13 <38968221+Joeymondello13@users.noreply.github.com>
2019-04-27 13:01:32 -0500

    Revert "Revert "Merge branch 'SFX!'""
    This reverts commit 9f67f66165f9ebed48305bf0c08e62e523deb86d.

--- AegisCannon/Assets/Scripts/EnemyFire.cs
+++ AegisCannon/Assets/Scripts/EnemyFire.cs
@@ -17,12 +17,15 @@ public class EnemyFire : MonoBehaviour
     public bool shouldFire = true;
     public int enemyShipFire;
     public static bool shipAlive = true;
+    //Object to play shot SFX - DH
+    public SFXscript playShot;
 
     // Start is called before the first frame update. Gets Animator Component and Starts infinite Coroutine to make enemies fire.
     void Start()
     {
         animator = GetComponent<Animator>();
         StartCoroutine(StartFire());
+        playShot = GameObject.FindObjectOfType(typeof(SFXscript)) as SFXscript;
     }
 
     // Method to make enemies fire a projectile upwards relative to their position.
@@ -30,6 +33,7 @@ public class EnemyFire : MonoBehaviour
     {
         Rigidbody2D projectile1 = Instantiate(projectileBody, shipTransform.position, shipTransform.rotation);
         projectile1.velocity = transform.up * projectileSpeed;
+        playShot.PlayEnemyFire();
     }
     // Called once per frame. Checks which enemy ship was assigned to fire. Sets all other enemy ships to not fire.
     void Update()
